ee90abf54d1be881fff0b7c2d827572c89830fd1
Tejasvini Dhoble <124334652+tejasvinidhoble@users.noreply.github.com>
2025-07-15 10:18:53 +0530

    Time: 1 ms (99.12%) | Memory: 42.1 MB (47.02%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char ch : word.toCharArray()) {
+            
+            if (!Character.isLetterOrDigit(ch)) {
+                return false;
+            }
+
+          
+            if (Character.isLetter(ch)) {
+                char lower = Character.toLowerCase(ch);
+                if ("aeiou".indexOf(lower) >= 0) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
