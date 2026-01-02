6d0cf594841cb15705d2b2101a69edd908d14a0f
Tejasvini Dhoble <124334652+tejasvinidhoble@users.noreply.github.com>
2026-01-02 20:33:42 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (25.57%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+         HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }
+            set.add(num);
+        }
+
+        return -1;
+    }
+}
